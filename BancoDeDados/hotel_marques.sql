2af14a9130e2c05b25f774ed8dd3936031f7401f
nataliafirmino <nataliaffirmino2401@gmail.com>
2024-11-24 21:37:24 -0300

    commit final

--- BancoDeDados/hotel_marques.sql
+++ BancoDeDados/hotel_marques.sql
@@ -1,14 +1,15 @@
-CREATE DATABASE hotelMarques;
-USE hotelMarques;
+CREATE DATABASE hotel_marques;
+USE hotel_marques;
 
 CREATE TABLE Usuario(
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(100) NOT NULL,
 email VARCHAR(100) NOT NULL,
-cpf VARCHAR(15) NOT NULL,
-senha VARCHAR(10) NOT NULL,
-cargo VARCHAR(50) NOT NULL,
-statusUsuario VARCHAR(20) /* Indica se o usuário está ativo ou não*/
+cpf VARCHAR (20) NOT NULL,
+senha VARCHAR(50) NOT NULL,
+cargo VARCHAR(100) NOT NULL,
+statusUsuario VARCHAR(255) NOT NULL,
+foto VARCHAR(255)
 );
 
 CREATE TABLE Cliente(
